d03965464d0108795b280bf4a505c1b18195156d
travis smith <travis86smith@gmail.com>
2019-05-23 05:47:05 +0000

    FactoryBot

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -20,11 +20,7 @@ RSpec.describe GramsController, type: :controller do
 
 
     it "should successfully show the new form" do
-        user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
       get :new
@@ -40,11 +36,7 @@ RSpec.describe GramsController, type: :controller do
 
 
     it "Should successfully create a new gram in our database" do
-        user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
       post :create, params: { gram: { message: 'Hello!'}}
       expect(response).to redirect_to root_path
@@ -55,11 +47,7 @@ RSpec.describe GramsController, type: :controller do
 
     end
     it "should properly deal with validation errors" do
-        user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-      )
+      user = FactoryBot.create(:user)
       sign_in user
 
       post :create, params: { gram: { message: '' } }
