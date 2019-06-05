484af3169023e3bbaff337f37ddca332fd20ce83
travis smith <travis86smith@gmail.com>
2019-06-05 05:10:51 +0000

    Lesson 15

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,16 +1,19 @@
 class GramsController < ApplicationController
-before_action :authenticate_user!, only: [:new, :create] 
+before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy] 
 
   def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user 
     @gram.destroy
     redirect_to root_path
   end
 
 def update
   @gram = Gram.find_by_id(params[:id])
-   return render_not_found if @gram.blank?
+  return render_not_found if @gram.blank?
+  return render_not_found(:forbidden) if @gram.user != current_user
+  
   @gram.update_attributes(gram_params)
   if @gram.valid?
     redirect_to root_path
@@ -28,7 +31,8 @@ def show
 def edit
   @gram = Gram.find_by_id(params[:id])
   return render_not_found if @gram.blank?
-  end
+  return render_not_found(:forbidden) if @gram.user != current_user
+end
 
 def new
   @gram = Gram.new
@@ -57,6 +61,9 @@ def render_not_found
   render plain: 'Not Found :(', status: :not_found
 end
 
+  def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
+  end
 
 end
 
